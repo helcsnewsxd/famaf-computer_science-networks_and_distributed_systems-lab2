31fe68c6645fd7d2ccb461ce6317fb41e5af73b2
Juanbratti <juanbratti@mi.unc.edu.ar>
2023-04-11 00:44:26 +0000

    re-restored server.py

--- server.py
+++ server.py
@@ -11,6 +11,8 @@ import optparse
 import socket
 import connection
 from constants import *
+import threading
+from queue import Queue
 
 
 class Server(object):
@@ -19,13 +21,18 @@ class Server(object):
   especificados donde se reciben nuevas conexiones de clientes.
   """
 
-  def __init__(self, addr=DEFAULT_ADDR, port=DEFAULT_PORT,
-               directory=DEFAULT_DIR):
+  def __init__(self, addr=DEFAULT_ADDR, port=DEFAULT_PORT, directory=DEFAULT_DIR):
     # Creación y configuración del socket
-    print("Serving %s on %s:%s." % (directory, addr, port))
-    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.socket.bind((addr, port))
+    print(f"Serving {directory} on {addr}:{port}.")
+    # Dirección y puerto donde escuchar, + directorio
+    self.host = addr
+    self.port = port
     self.directory = directory
+    # Se crea el socket para enlazar la conexión
+    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Se configura el socket para que no espere a que una conexión
+    # para volver a ser utilizado
+    self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
   def serve(self):
     """
@@ -34,17 +41,49 @@ class Server(object):
     """
     # Comienza escucha del socket
     # Solo puede atender a MAX_CLIENT clientes en simultáneo
+    self.socket.bind((self.host, self.port))
     self.socket.listen(MAX_CLIENT)
+
+    # Grupo de hilos
+    threads = []
+    # Cola de conexiones
+    connection_queue = Queue()
+
+    # Procesa conexiones entrantes
+    def handle():
+      while True:
+        # Obtiene una conexión de la cola
+        nw_socket, client_info = connection_queue.get()
+        print(f"Connection from {client_info[0]} using port {client_info[1]}")
+        # Creo la conexión con el cliente
+        new_connection = connection.Connection(nw_socket, self.directory)
+        # Procesa pedidos del cliente
+        new_connection.handle()
+        # Cierra conexión luego de procesar los pedidos
+        nw_socket.close()
+        print(
+            f"Connection from {client_info[0]} using port {client_info[1]} closed.")
+        # Indica que la conexión fue procesada, da a lugar a otra conexión
+        connection_queue.task_done()
+
+    # Crea los hilos para la cantidad de clientes máxima
+    for i in range(MAX_CLIENT):
+      # Cada hilo ejecuta la función handle
+      thr = threading.Thread(target=handle)
+      # Hilo termina cuando el programa termina
+      thr.daemon = True
+      # Inicia el hilo
+      thr.start()
+      # Agrega el hilo a la lista de hilos
+      threads.append(thr)
+
+    # Espera a que se procesen todas las conexiones
     while True:
-      # Acepto la conexión
-      # c es el socket de conexión
-      # a es (addr, port) del cliente
+      # Acepta una conexión
       nw_socket, client_info = self.socket.accept()
-      print(f"Connection from {client_info[0]} using port {client_info[1]}")
-      # Creo la conexión
-      new_connection = connection.Connection(nw_socket, self.directory)
-      # Resuelvo pedidos del cliente hasta que se vaya
-      new_connection.handle()
+      # Agrega la conexión a la cola para que sea procesada
+      # por un hilo en handle()
+      connection_queue.put((nw_socket, client_info))
 
 
 def main():
